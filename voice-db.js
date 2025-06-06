32bfc0641fc725e572ec261a89943592dfb2c319
Gaetano3187 <gaetano.gargano87@gmail.com>
2025-06-06 10:47:33 -0700

    Create voice-db.js

---
+++ voice-db.js
@@ -0,0 +1,23 @@
+window.commandsDB = {
+  add: [
+    "aggiungi", "metti", "inserisci", "compra", "acquista",
+    "aggiungimi", "aggiungere", "mettilo", "metti in lista", "da comprare",
+    "vorrei", "mettile", "da acquistare", "aggiungilo", "mettimelo in lista",
+    "mi serve", "porta a casa", "devo comprare", "vorrei comprare", "comprami"
+  ],
+  remove: [
+    "rimuovi", "togli", "elimina", "cancella", "butta",
+    "eliminalo", "rimuovilo", "elimina dalla lista", "cancellalo", "toglimelo"
+  ],
+  markPurchased: [
+    "comprato", "acquistato", "preso", "fatto", "l'ho preso",
+    "già preso", "segnalo acquistato", "segnalo come comprato",
+    "segnalo come fatto", "preso e pagato"
+  ],
+  clearList: [
+    "svuota", "cancella tutto", "pulisci", "svuota la lista", "rimuovi tutto"
+  ],
+  showList: [
+    "mostra lista", "cosa devo comprare", "lista della spesa", "mostrami la lista"
+  ]
+};
